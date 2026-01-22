22fa47fb8b6a2c80e48065e74f5abdc254fa03fa
mickaelluantrap-cyber <mickaelluantrap@gmail.com>
2026-01-22 11:23:35 -0300

    Add files via upload

---
+++ recapitulação/script.js
@@ -0,0 +1,14 @@
+
+function caulcular(){
+
+    let input_esquerdo = document.getElementById('input-esquerdo')
+    let input_direito = document.getElementById('input-direito')
+    let select_operacao = document.getElementById('operacao')
+
+    let valor_esquerdo = input_esquerdo.value
+    let valor_direito = input_direito.value
+    let operacao = select_operacao.value
+
+    alert('valor A: $
+
+}
